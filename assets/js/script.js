7a755a29d1a4e90b5181a481849ea85fd587eb0e
gabrielepetralia <gabri.petralia03@gmail.com>
2023-03-17 13:31:11 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/*----- Passaggi -----*/
+/* 
+  1 - Chiedere all'utente il suo nome e salvarlo in una variabile; 
+  2 - Chiedere all'utente il suo cognome e salvarlo in una variabile;
+  3 - Chiedere all'utente il suo colore preferito e poi salvarlo in una variabile;
+  4 - Chiedere all'utente la sua età e salvarla in una variabile;
+  5 - Scrivere sulla pagina la password formata dall'insieme di tutte le variabili.
+*/ 
+
+console.log("test");
